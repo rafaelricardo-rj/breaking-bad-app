2827c2419514f669064c6df4a0b644f196fc18ab
Rafael Ricardo <rafael_rikardo@yahoo.com.br>
2020-04-13 23:08:39 +0000

    Imported HttpClient module and get request for testing printing results to the console.log.

--- src/app/pages/cast/cast.page.ts
+++ src/app/pages/cast/cast.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cast',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CastPage implements OnInit {
 
-  constructor() { }
+    cast: Observable<any>;
+    constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+	
+        this.cast = this.http.get('https://www.breakingbadapi.com/api/characters');
+	
+        this.cast.subscribe(data => {
+	
+            console.log('my data: ', data);
+	
+        });
+    }
 
 }
