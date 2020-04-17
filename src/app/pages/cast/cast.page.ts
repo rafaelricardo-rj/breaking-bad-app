9b2b3b1f07ed31862e8bb9d81701562a5f75b7d6
Rafael Ricardo <rafael_rikardo@yahoo.com.br>
2020-04-17 11:33:57 +0000

    Trying to fetch paginated characters from Api. Not working fully yet.

--- src/app/pages/cast/cast.page.ts
+++ src/app/pages/cast/cast.page.ts
@@ -15,8 +15,24 @@ export class CastPage implements OnInit {
     constructor(private router: Router, private http: HttpClient) {}
 
     ngOnInit() {
-        this.characters = this.http.get('https://www.breakingbadapi.com/api/characters');
+        this.characters = this.http.get('https://www.breakingbadapi.com/api/characters?limit=15&offset=0');
         //this.characters = this.http.get('../servicos/characters.json');
+        this.characters.subscribe(data => console.log(data.length));
+    }
+
+    loadData(event){
+        
+        this.characters = this.http.get('https://www.breakingbadapi.com/api/characters?limit=15&offset=15');
+        
+        event.target.complete();
+
+            // App logic to determine if all data is loaded
+            // and disable the infinite scroll
+            /*if (this.loadedData.length > 30) {
+                this.util.presentToast('No more data available', true, 'bottom', 1500);
+                event.target.disabled = true;
+            }*/
+        this.characters.subscribe(data => console.log('repopulado '+data.length));
     }
 
 }
